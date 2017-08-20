fd957e5af4ed1a539fb5a0b18edbdb60cc958e71
Robin Krenecky <rkrenecky@gmail.com>
2017-08-20 17:47:52 +0200

    Erases example, fixes directory

--- BadgeControlExample/Pods/Target Support Files/BadgeControl/BadgeControl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BadgeControl : NSObject
-@end
-@implementation PodsDummy_BadgeControl
-@end
--- BadgeControlExample/Pods/Target Support Files/Pods-BadgeControlExample/Pods-BadgeControlExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BadgeControlExample : NSObject
-@end
-@implementation PodsDummy_Pods_BadgeControlExample
-@end
